7a23dd3a0be6f41121e2ed7dcb3b89487fc7e7ea
Lijunyi <35330398+northxiruan@users.noreply.github.com>
2023-05-06 13:00:43 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -8,38 +8,23 @@ except ImportError as e:
     import numpy as np
 
 def ridge(data):
-   # 假设 'data' 是一个 n x m 的 numpy 数组
-    X = data[:, :-1]
-    y = data[:, -1]
-    XtX = np.dot(X.T, X)
-    XtXpI = XtX + np.eye(X.shape[1])
-    XtXpI_inv = np.linalg.inv(XtXpI)
-    XtY = np.dot(X.T, y)
-    w = np.dot(XtXpI_inv, XtY)
-    return w
+    X,y=read_data()
+    z=np.matmul(X.T,X)+np.eye(X.shape[1])*(-0.1)
+    weight=np.matmul(np.linalg.inv(z),np.matmul(X.T,y))
+    return weight @ data
     
-def lasso(data, alpha=1.0, max_iter=1000, tol=1e-4):
-    # 假设 'data' 是一个 n x m 的 numpy 数组
-    X = data[:, :-1]
-    y = data[:, -1]
-    w = np.zeros((X.shape[1], 1))
-    converged = False
-    iter_count = 0
-    while not converged and iter_count < max_iter:
-        w_old = w.copy()
-        for j in range(X.shape[1]):
-            a_j = 2 * np.sum(X[:, j]**2)
-            c_j = 2 * np.dot(X[:, j], y - np.dot(X, w) + w[j]*X[:, j])
-            if c_j < -alpha:
-                w[j] = (c_j + alpha) / a_j
-            elif c_j > alpha:
-                w[j] = (c_j - alpha) / a_j
-            else:
-                w[j] = 0
-        if np.linalg.norm(w - w_old) < tol:
-            converged = True
-        iter_count += 1
-    return w
+def lasso(data):
+    X,y=read_data()
+    m, n = X.shape
+    weight = np.array([ 1.49462254e+01, -2.50275342e-01, -8.76423816e-03,  1.23727270e+00,
+       -1.80224871e+02, -2.10165019e+02])
+    max_iterations = 100000
+    for i in range(max_iterations):
+       grad = (np.matmul(X.T, (np.matmul(X, weight) - y))) + 30 * np.sign(weight)
+       weight = weight - 1e-12 * grad
+       if np.linalg.norm(grad) < 0.0001:
+           break
+    return weight @ data
 
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
