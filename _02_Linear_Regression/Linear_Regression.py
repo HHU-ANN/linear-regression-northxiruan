0ff92cc691af39d18640916e3deb16892266491e
Lijunyi <35330398+northxiruan@users.noreply.github.com>
2023-05-06 13:44:59 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -8,21 +8,21 @@ except ImportError as e:
     import numpy as np
 #最小二乘岭回归
 def ridge(data):
-    X,y=read_data()
+    X,y=read_data() #读入数据
     z=np.matmul(X.T,X)+np.eye(X.shape[1])*(0.000000000000000000000000000001)
-    weight=np.matmul(np.linalg.inv(z),np.matmul(X.T,y))
+    weight=np.matmul(np.linalg.inv(z),np.matmul(X.T,y)) #最小二乘岭回归公式
     return weight @ data
-    #梯度下降Lasso回归 
+#梯度下降Lasso回归 
 def lasso(data):
     X,y=read_data()
     m, k = X.shape
-    weight=np.zeros(k)
-    max_iterations = 100000
+    weight=np.zeros(k) #初始化
+    max_iterations = 100000 #次数
     for i in range(max_iterations):
-       grad = (np.matmul(X.T, (np.matmul(X, weight) - y))) + 30 * np.sign(weight)
+       grad = (np.matmul(X.T, (np.matmul(X, weight) - y))) + 30 * np.sign(weight) #梯度引入公式
        weight = weight - 1e-11 * grad
         #学习率1e-11
-       if np.linalg.norm(grad) < 0.0001:
+       if np.linalg.norm(grad) < 0.0001:#边界
            break
     return weight @ data
 
