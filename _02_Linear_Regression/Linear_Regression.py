4e414d7590a4f9448943c906dddfaa73bc62921f
Lijunyi <35330398+northxiruan@users.noreply.github.com>
2023-05-06 13:02:45 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -9,15 +9,14 @@ except ImportError as e:
 
 def ridge(data):
     X,y=read_data()
-    z=np.matmul(X.T,X)+np.eye(X.shape[1])*(-0.1)
+    z=np.matmul(X.T,X)+np.eye(X.shape[1])*(0.000000000000000000000000000001)
     weight=np.matmul(np.linalg.inv(z),np.matmul(X.T,y))
     return weight @ data
     
 def lasso(data):
     X,y=read_data()
-    m, n = X.shape
-    weight = np.array([ 1.49462254e+01, -2.50275342e-01, -8.76423816e-03,  1.23727270e+00,
-       -1.80224871e+02, -2.10165019e+02])
+    m, k = X.shape
+    weight=np.zeros(k)
     max_iterations = 100000
     for i in range(max_iterations):
        grad = (np.matmul(X.T, (np.matmul(X, weight) - y))) + 30 * np.sign(weight)
