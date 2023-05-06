c5bfea1e1e0df7e148561ab65663c8a8e40f14fa
Lijunyi <35330398+northxiruan@users.noreply.github.com>
2023-05-06 12:55:53 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -3,30 +3,24 @@
 import os
 
 try:
-    import numpy as n
+    import numpy as np
 except ImportError as e:
     os.system("sudo pip3 install numpy")
-    import numpy as n
+    import numpy as np
 
 def ridge(data):
-    X,y=read_data()
-    z=n.matmul(X.T,X)+n.eye(X.shape[1])*(0.00000000000000001)
-    weight=n.matmul(n.linalg.inv(z),n.matmul(X.T,y))
-    return weight @ data
-    
+    x, y = read_data()
+    weight = np.dot(np.linalg.inv(np.dot(x.T, x)), np.dot(x.T, y))
+
+    retrun weight
+
 def lasso(data):
-    X,y=read_data()
-    m, k = X.shape
-    weight=n.zeros(k)
-    max_iterations = 100000
-    for i in range(max_iterations):
-       grad = (n.matmul(X.T, (n.matmul(X, weight) - y))) + 30 * n.sign(weight)
-       weight = weight - 1e-12 * grad
-       if n.linalg.norm(grad) < 0.0001:
-           break
-    return weight @ data
+    x, y = read_data()
+    weight = np.dot(np.linalg.inv(np.dot(x.T, x)), np.dot(x.T, y))
+
+    retrun weight
 
 def read_data(path='./data/exp02/'):
-    x = n.load(path + 'X_train.npy')
-    y = n.load(path + 'y_train.npy')
+    x = np.load(path + 'X_train.npy')
+    y = np.load(path + 'y_train.npy')
     return x, y
